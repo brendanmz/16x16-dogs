43b175464bb50a9c4ae2742e0dc8daf268f3447b
Brendan <brendanmoratz@hotmail.com>
2021-04-04 18:37:46 +0200

    Update number of images loaded and fade in timing

--- src/AllImages.tsx
+++ src/AllImages.tsx
@@ -9,10 +9,10 @@ function AllImages({ windowHeight, windowWidth }: Props) {
   const [allLoaded, setAllLoaded] = useState(false);
   const [loadedCount, setLoadedCount] = useState<Array<number>>([]);
 
+  /** Loads 36 random dog images */
   const recursiveFetch = async () => {
     try {
-      // loads 16 random dog images
-      const data = await fetch('https://dog.ceo/api/breeds/image/random/16');
+      const data = await fetch('https://dog.ceo/api/breeds/image/random/36');
       const response = await data.json();
       setImages(response.message);
     } catch (error) {
@@ -28,7 +28,7 @@ function AllImages({ windowHeight, windowWidth }: Props) {
   /** Set's allLoaded to true when all images have been received */
   const handleImagesLoaded = (index: number) => () => {
     setLoadedCount([...loadedCount, index]);
-    if (loadedCount.length === 15) {
+    if (loadedCount.length === 35) {
       setAllLoaded(true);
     }
   };
@@ -41,10 +41,10 @@ function AllImages({ windowHeight, windowWidth }: Props) {
             objectFit: 'cover',
             opacity: allLoaded ? 1 : 0,
             transition: `opacity 0.25s ease-in`,
-            transitionDelay: `${index / 8}s`,
+            transitionDelay: `${index / 16}s`,
           }}
-          width={Math.round(windowWidth / 4) - 4} // TODO: find better solution to avoid page wrapping
-          height={Math.round(windowHeight / 4)}
+          width={Math.round(windowWidth / 6) - 4} // TODO: find better solution to avoid page wrapping
+          height={Math.round(windowHeight / 6)}
           key={url}
           src={url}
           alt={`${index}`}
